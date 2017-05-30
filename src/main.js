68ae05e99df0b3c0a4c2719a3ddaa17170513bb7
Ansgar Mertens <a@amrtns.com>
2017-05-30 15:16:52 +0200

    add DetailScreen to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { StackNavigator } from 'react-navigation';
 import HomeScreen from './screens/Home';
+import DetailScreen from './screens/Detail';
 
 const MainApp = StackNavigator({
   Home: { screen: HomeScreen },
+  Detail: { screen: DetailScreen },
 });
 
 export default MainApp;
